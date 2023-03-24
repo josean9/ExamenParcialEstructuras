29e41a16b5c8f3f4b382a989c336d2fa9e66e3a5
jose antonio <jsancesc@gmail.com>
2023-03-24 12:07:44 +0100

    decorador retirada

--- Decoradores.py
+++ Decoradores.py
@@ -1,16 +1,12 @@
-def decorado1(funcion):
+def decoradorTransferencia(funcion):
     def funcion_decorada(*args, **kwargs):
         print("Se va a ejecutar la transferencia")
         return funcion(*args, **kwargs)
         print("Se ha ejecutado la transferencia")
     return funcion_decorada
-def decorador2(funcion):
+def decoradorRetirada(funcion):
     def funcion_decorada(*args, **kwargs):
-        print("Se va a ejecutar la transferencia")
+        print("Se va a ejecutar la retirada")
         return funcion(*args, **kwargs)
         print("Se ha ejecutado la transferencia")
     return funcion_decorada
-@decorado1
-def hola():
-    print("Hola")
-print(hola())
